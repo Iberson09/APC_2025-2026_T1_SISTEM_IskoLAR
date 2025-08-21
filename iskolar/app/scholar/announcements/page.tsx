7c3f79a404854ef1143d9abbba746f3593e52e79
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-08-21 17:51:14 +0800

    added announcements page & layout

---
+++ iskolar/app/scholar/announcements/page.tsx
@@ -0,0 +1,10 @@
+export default function AnnouncementsPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4 ml-64">Announcements</h1>
+      <p className="text-gray-700 ml-64">
+        This is the <strong>Announcements</strong> page.
+      </p>
+    </div>
+  );
+}

5e2d648c5d67482e4b9e3935e8b23f1f5beef6da
Hazel Ann Mones <hbmones@student.apc.edu.ph>
2025-10-24 10:42:34 +0800

    deleted files

--- iskolar/app/admin/applications/[id]/education/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import EducationTab from '../components/EducationTab';
-import { applicationData } from '../layout';
-
-export default function EducationPage() {
-  return <EducationTab />;
-}
--- iskolar/app/admin/applications/[id]/eligibility/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import EligibilityTab from '../components/EligibilityTab';
-import { applicationData } from '../layout';
-
-export default function EligibilityPage() {
-  return <EligibilityTab />;
-}
